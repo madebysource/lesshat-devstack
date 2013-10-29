3cec36e4d4ec2e84e8bffcaa2d5d3f0272a0c7e6
Petr Brzek <petrbrzek@abdoc.net>
2013-10-29 18:50:35 +0100

    name fix

--- src/test-env.js
+++ src/test-env.js
@@ -5,7 +5,7 @@ var TestSuite = require('./test-suite');
 var tester = new TestSuite();
 
 var parent_dirname = path.resolve(__dirname, '..', '..', '..');
-var build_result_path = path.join(parent_dirname, 'build', 'mixins.less');
+var build_result_path = path.join(parent_dirname, 'build', 'lesshat.less');
 tester.addBuildResult(build_result_path);
 
 global.test = tester;
