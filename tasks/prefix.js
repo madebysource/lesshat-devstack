36dcc9d05250ff2385ce874c3758fc74c650ae85
Petr Brzek <petrbrzek@abdoc.net>
2013-11-02 01:07:57 +0100

    grunt task edited

--- tasks/prefix.js
+++ tasks/prefix.js
@@ -3,11 +3,11 @@ module.exports = function (grunt) {
     var path = path_global.join(__dirname, '/build/');
     // Check whether build folder exists
     if (!fs.existsSync(path)) {
-      grunt.fail.fatal('Mixins folder does not exist.');
+      grunt.fail.fatal('Build folder does not exist.');
     }
     // Check whether lesshat.less exists
     if (!fs.existsSync(path + 'lesshat.less')) {
-      grunt.fail.fatal('Mixin template does not exist.');
+      grunt.fail.fatal('lesshat.less does not exist.');
     }
     var mixins = fs.readFileSync(path + 'lesshat.less', 'utf8');
     mixins = mixins.replace(/^\.([a-z0-9-]+\([a-z@., 0-9-]+)/gm, function (match, sub_1) { return '.lh-' + sub_1; });
