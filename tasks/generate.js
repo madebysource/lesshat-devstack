59c2b874432f9d111f44a5920855e5c5667dd618
Petr Brzek <petrbrzek@abdoc.net>
2013-11-03 01:05:56 +0100

    generate documentation automatically

--- tasks/generate.js
+++ tasks/generate.js
@@ -35,13 +35,15 @@ module.exports = function(grunt) {
     }
 
     function fileCreator(mixin_name, path, data, exported, done) {
-      var mixin_template_path = path.global.join(path.template, mixin_name + '.js');
-      var mixin_template = fs.readFileSync(mixin_template_path, 'utf8');
+      if (mixin_name) {
+        var mixin_template_path = path.global.join(path.template, mixin_name + '.js');
+        var mixin_template = fs.readFileSync(mixin_template_path, 'utf8');
 
-      mixin_template = mixin_template.replace(/{{\s*(\w+\s*\|?\s*\w+)\s*}}/g, function(match, variable) {
-        return (!(config[variable] instanceof Array) && (config[variable])) || (JSON.stringify(data[variable]).replace(/"/g, '\''));
-      });
-      fs.writeFileSync(path.global.join(path.local, exported.path, exported.file + '.js'), mixin_template);
+        mixin_template = mixin_template.replace(/{{\s*(\w+\s*\|?\s*\w+)\s*}}/g, function(match, variable) {
+          return (!(config[variable] instanceof Array) && (config[variable])) || (JSON.stringify(data[variable]).replace(/"/g, '\''));
+        });
+      }
+      fs.writeFileSync(path.global.join(path.local, exported.path, exported.file), mixin_template || '##');
 
       if (done) done();
     }
@@ -52,7 +54,7 @@ module.exports = function(grunt) {
       template: lesshatdev_path
     }, config, {
       path: config.mixin_css_name,
-      file: config.mixin_css_name
+      file: config.mixin_css_name + '.js'
     }, function() {
       grunt.log.ok('Mixin file created – DONE');
     });
@@ -63,9 +65,20 @@ module.exports = function(grunt) {
       template: lesshatdev_path
     }, config, {
       path: config.mixin_css_name,
-      file: 'test'
+      file: 'test.js'
     }, function() {
       grunt.log.ok('Test file created – DONE');
+    });
+
+    fileCreator('documentation-template', {
+      global: path_global,
+      local: path,
+      template: lesshatdev_path
+    }, config, {
+      path: config.mixin_css_name,
+      file: config.mixin_css_name + '.md'
+    }, function() {
+      grunt.log.ok('Document file created – DONE');
     });
 
   });
